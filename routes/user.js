e53133f729860ca616ef915ad911d3a4a1f69487
Kevin-Clement <kevin-clement@live.fr>
2022-03-22 19:30:51 +0100

    Routes

---
+++ routes/user.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+const userCtrl = require('../controllers/user');
+
+
+router.post('/signup', userCtrl.signUp);
+router.post('/login', userCtrl.login);
+router.get('/users', userCtrl.getAllUser);
+
+
+module.exports = router;
